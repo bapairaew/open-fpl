61e9364e0368bd732167acde30685e29779d4e84
Narudom <iam@bapairaew.com>
2021-05-10 20:03:50 +0700

    Remove dark mode

--- theme.js
+++ theme.js
@@ -11,38 +11,6 @@ const theme = {
     danger: "rgb(255, 23, 81)",
     warning: "rgb(255, 171, 27)",
     success: "rgb(1, 252, 122)",
-    modes: {
-      dark: {
-        text: "#fff",
-        background: "#060606",
-        primary: "#3cf",
-        secondary: "#3cf",
-        muted: "#191919",
-        highlight: "#3cf",
-        gray: "#999",
-        accent: "#c0f",
-      },
-      deep: {
-        text: "hsl(210, 50%, 96%)",
-        background: "hsl(230, 25%, 18%)",
-        primary: "hsl(260, 100%, 80%)",
-        secondary: "hsl(290, 100%, 80%)",
-        highlight: "hsl(260, 20%, 40%)",
-        accent: "hsl(290, 100%, 80%)",
-        muted: "hsla(230, 20%, 0%, 20%)",
-        gray: "hsl(210, 50%, 60%)",
-      },
-      swiss: {
-        text: "hsl(10, 20%, 20%)",
-        background: "hsl(10, 10%, 98%)",
-        primary: "hsl(10, 80%, 50%)",
-        secondary: "hsl(10, 60%, 50%)",
-        highlight: "hsl(10, 40%, 90%)",
-        accent: "hsl(250, 60%, 30%)",
-        muted: "hsl(10, 20%, 94%)",
-        gray: "hsl(10, 20%, 50%)",
-      },
-    },
   },
   fonts: {
     body:
