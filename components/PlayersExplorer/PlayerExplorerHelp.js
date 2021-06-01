cd1bb5398c19687a95a7be778354845682495574
Narudom <iam@bapairaew.com>
2021-06-01 21:24:26 +0700

    Improve Player Explorer help

--- components/PlayersExplorer/PlayerExplorerHelp.js
+++ components/PlayersExplorer/PlayerExplorerHelp.js
@@ -1,5 +1,6 @@
 import {
   Box,
+  Container,
   Flex,
   Heading,
   Link as A,
@@ -176,23 +177,44 @@ const gameweeks = [
 
 const PlayerExplorerHelp = () => {
   return (
-    <Box>
-      <Heading as="h1" size="lg" fontWeight="black">
-        Player Explorer
-      </Heading>
-      <Text my={4} as="p">
-        This is how to use{" "}
-        <Link href="/" passHref>
-          <A>Player Explorer</A>
-        </Link>{" "}
-        page.
-      </Text>
-      <Heading my={4} as="h2" size="md" fontWeight="black">
-        Player Card
-      </Heading>
-      <Text my={4} as="p">
-        Below is the anatomy of a player card.
-      </Text>
+    <>
+      <Container my={4}>
+        <Heading as="h1" size="lg" fontWeight="black">
+          Player Explorer
+        </Heading>
+        <Text my={4} as="p">
+          This page explains the data embeded in Player Card and how to use each
+          component in{" "}
+          <Link href="/" passHref>
+            <A color="brand.500">Player Explorer</A>
+          </Link>{" "}
+          page.
+        </Text>
+        <Heading my={4} as="h2" size="md" fontWeight="black">
+          Player Card
+        </Heading>
+        <Text my={4} as="p">
+          Player Card consists of some relevant statistic data that you would
+          gernerally want to know in order to decide if you want to include the
+          player in your team or not.
+        </Text>
+        <Text my={4} as="p">
+          The data is scraped from{" "}
+          <A
+            color="brand.500"
+            isExternal
+            href="https://fantasy.premierleague.com/"
+          >
+            FPL
+          </A>{" "}
+          and{" "}
+          <A color="brand.500" isExternal href="https://understat.com/">
+            Understat
+          </A>{" "}
+          and updated once a day around 02:00 AM - 03:00 GMT+0.
+        </Text>
+      </Container>
+
       <Flex
         mx="auto"
         justifyContent="center"
@@ -318,110 +340,113 @@ const PlayerExplorerHelp = () => {
         </Box>
       </Flex>
 
-      <Heading my={4} as="h2" size="md" fontWeight="black">
-        Player Search Bar
-      </Heading>
+      <Container my={4}>
+        <Heading my={4} as="h2" size="md" fontWeight="black">
+          Player Search Bar
+        </Heading>
 
-      <Heading my={4} as="h3" size="sm" fontWeight="black">
-        Filtering
-      </Heading>
+        <Heading my={4} as="h3" size="sm" fontWeight="black">
+          Filtering
+        </Heading>
 
-      <Text my={4} as="p">
-        Player Search Bar supports free text search which will do fuzzy search
-        on players' full name hence if you typed "Paul", the search results
-        would show (Paul) Pogba and (Paul) Dummett.
-      </Text>
-      <Box my={4} pointerEvents="none">
-        <PlayerSearchBar initialSeachQuery="Paul" />
-      </Box>
-      <Text my={4} as="p">
-        It also supports search query syntax like when you do email search. For
-        example, if you want to specifically find a player based on their name
-        FPL page you can use field search <code>name</code> like shown below:
-      </Text>
-      <Box my={4} pointerEvents="none">
-        <PlayerSearchBar initialSeachQuery="name:Pogba" />
-      </Box>
-      <Box mt={4} mb={2}>
-        Currently, it supports the following fields:
-        <UnorderedList my={4} ml={6}>
-          <ListItem my={2}>
-            <Text>
-              <code>id</code> - FPL ID
-            </Text>
-            <Box my={4} pointerEvents="none">
-              <PlayerSearchBar initialSeachQuery="id:296" />
-            </Box>
-          </ListItem>
-          <ListItem my={2}>
-            <Text>
-              <code>name</code> - FPL web name
-            </Text>
-            <Box my={4} pointerEvents="none">
-              <PlayerSearchBar initialSeachQuery="name:Pogba" />
-            </Box>
-          </ListItem>
-          <ListItem my={2}>
-            <Text>
-              <code>cost</code> - FPL player's cost range
-            </Text>
-            <Box my={4} pointerEvents="none">
-              <PlayerSearchBar initialSeachQuery="cost:4-10" />
-            </Box>
-          </ListItem>
-          <ListItem my={2}>
-            <Text>
-              <code>position</code> - FPL positon (FWD, MID, DEF, GKP)
-            </Text>
-            <Box my={4} pointerEvents="none">
-              <PlayerSearchBar initialSeachQuery="position:MID" />
-            </Box>
-          </ListItem>
-          <ListItem my={2}>
-            <Text>
-              <code>team</code> - FPL short team name (e.g. MUN, LIV, MCI)
-            </Text>
-            <Box my={4} pointerEvents="none">
-              <PlayerSearchBar initialSeachQuery="team:MUN" />
-            </Box>
-          </ListItem>
-        </UnorderedList>
-      </Box>
-      <Text my={4} as="p">
-        You can also do field keyword combination to expand your search results,
-        for example, if you want to search for players from Manchester United OR
-        Liverpool you can do the following:
-      </Text>
-      <Box my={4} pointerEvents="none">
-        <PlayerSearchBar initialSeachQuery="name:MUN,LIV" />
-      </Box>
-      <Text my={4} as="p">
-        Or if you want to narrow down your search result you could append anoter
-        field search query, for example, the following will show Manchester
-        United midfield players
-      </Text>
-      <Box my={4} pointerEvents="none">
-        <PlayerSearchBar initialSeachQuery="team:MUN position:MID" />
-      </Box>
-      <Text my={4} as="p">
-        You can also exclude some search results use exclusion prefix `-` like
-        shown below how to exclude Pogba from search results. Please also note
-        that range search (e.g. cost) DOES NOT support exclusion yet.
-      </Text>
-      <Box my={4} pointerEvents="none">
-        <PlayerSearchBar initialSeachQuery="team:MUN position:MID -name:Pogba" />
-      </Box>
+        <Text my={4} as="p">
+          Player Search Bar supports free text search which will do fuzzy search
+          on players' full name hence if you typed "Paul", the search results
+          would show (Paul) Pogba and (Paul) Dummett.
+        </Text>
+        <Box my={4} pointerEvents="none">
+          <PlayerSearchBar initialSeachQuery="Paul" />
+        </Box>
+        <Text my={4} as="p">
+          It also supports search query syntax like when you do email search.
+          For example, if you want to specifically find a player based on their
+          name FPL page you can use field search <code>name</code> like shown
+          below:
+        </Text>
+        <Box my={4} pointerEvents="none">
+          <PlayerSearchBar initialSeachQuery="name:Pogba" />
+        </Box>
+        <Box mt={4} mb={2}>
+          Currently, it supports the following fields:
+          <UnorderedList my={4} ml={6}>
+            <ListItem my={2}>
+              <Text>
+                <code>id</code> - FPL ID
+              </Text>
+              <Box my={4} pointerEvents="none">
+                <PlayerSearchBar initialSeachQuery="id:296" />
+              </Box>
+            </ListItem>
+            <ListItem my={2}>
+              <Text>
+                <code>name</code> - FPL web name
+              </Text>
+              <Box my={4} pointerEvents="none">
+                <PlayerSearchBar initialSeachQuery="name:Pogba" />
+              </Box>
+            </ListItem>
+            <ListItem my={2}>
+              <Text>
+                <code>cost</code> - FPL player's cost range
+              </Text>
+              <Box my={4} pointerEvents="none">
+                <PlayerSearchBar initialSeachQuery="cost:4-10" />
+              </Box>
+            </ListItem>
+            <ListItem my={2}>
+              <Text>
+                <code>position</code> - FPL positon (FWD, MID, DEF, GKP)
+              </Text>
+              <Box my={4} pointerEvents="none">
+                <PlayerSearchBar initialSeachQuery="position:MID" />
+              </Box>
+            </ListItem>
+            <ListItem my={2}>
+              <Text>
+                <code>team</code> - FPL short team name (e.g. MUN, LIV, MCI)
+              </Text>
+              <Box my={4} pointerEvents="none">
+                <PlayerSearchBar initialSeachQuery="team:MUN" />
+              </Box>
+            </ListItem>
+          </UnorderedList>
+        </Box>
+        <Text my={4} as="p">
+          You can also do field keyword combination to expand your search
+          results, for example, if you want to search for players from
+          Manchester United OR Liverpool you can do the following:
+        </Text>
+        <Box my={4} pointerEvents="none">
+          <PlayerSearchBar initialSeachQuery="name:MUN,LIV" />
+        </Box>
+        <Text my={4} as="p">
+          Or if you want to narrow down your search result you could append
+          anoter field search query, for example, the following will show
+          Manchester United midfield players
+        </Text>
+        <Box my={4} pointerEvents="none">
+          <PlayerSearchBar initialSeachQuery="team:MUN position:MID" />
+        </Box>
+        <Text my={4} as="p">
+          You can also exclude some search results use exclusion prefix `-` like
+          shown below how to exclude Pogba from search results. Please also note
+          that range search (e.g. cost) DOES NOT support exclusion yet.
+        </Text>
+        <Box my={4} pointerEvents="none">
+          <PlayerSearchBar initialSeachQuery="team:MUN position:MID -name:Pogba" />
+        </Box>
 
-      <Heading my={4} as="h3" size="sm" fontWeight="black">
-        Sorting
-      </Heading>
+        <Heading my={4} as="h3" size="sm" fontWeight="black">
+          Sorting
+        </Heading>
 
-      <Text my={4} as="p">
-        You can sort the results by using the dropdown besides the search text
-        box. The default is "Best Fixtures" which is calculated from FPL match
-        difficulty.
-      </Text>
-    </Box>
+        <Text my={4} as="p">
+          You can sort the results by using the dropdown besides the search text
+          box. The default is "Best Fixtures" which is calculated from FPL match
+          difficulty.
+        </Text>
+      </Container>
+    </>
   );
 };
 
--- components/TransferPlanner/TransferPlannerHelp.js
+++ components/TransferPlanner/TransferPlannerHelp.js
@@ -1,5 +1,19 @@
+import { Text, Container, Heading } from "@chakra-ui/react";
+
 const TransferPlannerHelp = () => {
-  return "TODO";
+  return (
+    <Container my={4}>
+      <Heading as="h1" size="lg" fontWeight="black">
+        Transfer Planner
+      </Heading>
+      <Text my={4} as="p">
+        Transfer Planner is where you plan your transfer for upcoming gameweeks.
+      </Text>
+      <Text my={4} as="p">
+        It is still work-in-progress.
+      </Text>
+    </Container>
+  );
 };
 
 export default TransferPlannerHelp;
