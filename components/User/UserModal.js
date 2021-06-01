96c9bb6804301f5541397a43fae555e6acc3e597
Narudom <iam@bapairaew.com>
2021-06-01 17:05:43 +0700

    Find team id guide

--- components/User/UserModal.js
+++ components/User/UserModal.js
@@ -9,13 +9,50 @@ import {
   DrawerHeader,
   DrawerOverlay,
   FormLabel,
+  Icon,
+  IconButton,
   Input,
+  InputGroup,
+  InputRightElement,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTrigger,
+  Portal,
   useToast,
+  Link,
 } from "@chakra-ui/react";
 import { useEffect, useRef, useState } from "react";
+import { IoHelpCircleOutline, IoOpenOutline } from "react-icons/io5";
 import { getUserTeamChangesKey } from "~/components/User/storage";
 import { useUser } from "~/components/User/UserContext";
 import UserStorage from "~/components/User/UserStorage";
+
+const TeamIDHelpButton = () => (
+  <Popover>
+    <PopoverTrigger>
+      <IconButton
+        variant="ghost"
+        icon={<Icon aria-label="help" as={IoHelpCircleOutline} />}
+      />
+    </PopoverTrigger>
+    <PopoverContent>
+      <PopoverArrow />
+      <PopoverCloseButton />
+      <PopoverHeader>Find your team ID</PopoverHeader>
+      <PopoverBody>
+        Don't know where to find it? Just follow this{" "}
+        <Link color="brand.500" href="https://fpl.team/find-id" isExternal>
+          find your team ID guide <Icon as={IoOpenOutline} />
+        </Link>
+        !
+      </PopoverBody>
+    </PopoverContent>
+  </Popover>
+);
 
 const UserModal = ({ isOpen, onClose }) => {
   const toast = useToast();
@@ -83,15 +120,18 @@ const UserModal = ({ isOpen, onClose }) => {
         <DrawerBody>
           <form id="set-up-form" onSubmit={onSubmit}>
             <FormLabel htmlFor="teamId">Team ID</FormLabel>
-            <Input
-              ref={initialFocusRef}
-              id="teamId"
-              placeholder="e.g. 254181"
-              value={formTeamId}
-              onChange={(e) => setFormTeamId(e.target.value)}
-            />
+
+            <InputGroup>
+              <Input
+                ref={initialFocusRef}
+                id="teamId"
+                placeholder="e.g. 254181"
+                value={formTeamId}
+                onChange={(e) => setFormTeamId(e.target.value)}
+              />
+              <InputRightElement children={<TeamIDHelpButton />} />
+            </InputGroup>
           </form>
-          {/* TODO: Team ID help */}
           <Box my={4}>
             <FormLabel>Storage</FormLabel>
             <UserStorage
