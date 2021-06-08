7af20e9f1cf5b0bbe3b64af8d632412c8faa20ee
Narudom <iam@bapairaew.com>
2021-06-08 14:04:05 +0700

    Fix screen broken with scrollbar turned on

--- components/PlayersExplorer/PlayersExplorer.js
+++ components/PlayersExplorer/PlayersExplorer.js
@@ -53,7 +53,7 @@ const PlayersExplorer = ({ players, gameweeks, columnsSettings }) => {
   );
 
   return (
-    <>
+    <Box overflow="hidden" height="100%">
       <Box px={4} py={2}>
         <PlayerSearchBar onResults={setDisplayedPlayers} players={players} />
       </Box>
@@ -69,7 +69,7 @@ const PlayersExplorer = ({ players, gameweeks, columnsSettings }) => {
           </List>
         )}
       </AutoSizer>
-    </>
+    </Box>
   );
 };
 
