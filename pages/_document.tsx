a8a55bb9c6f82e1bc9fbfe66c772a9d6481460a1
Narudom <iam@bapairaew.com>
2021-07-14 10:44:36 +0700

    Favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,46 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import theme from "~/theme";
 
 class AppDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicons/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/favicons/safari-pinned-tab.svg"
+            color={theme.colors.brand[500]}
+          />
+          <meta name="apple-mobile-web-app-title" content="Open FPL" />
+          <meta name="application-name" content="Open FPL" />
+          <meta
+            name="msapplication-TileColor"
+            content={theme.colors.brand[500]}
+          />
+          <meta
+            name="msapplication-TileImage"
+            content="/favicons/mstile-144x144.png"
+          />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
         <body>
           <Main />
           <NextScript />
---
+++ scripts/make-cli-real-favicon-config.ts
@@ -0,0 +1,75 @@
+import fs from "fs";
+import theme from "~/theme";
+
+const config = {
+  masterPicture: "public/logo.svg",
+  iconsPath: "/favicons/",
+  design: {
+    ios: {
+      pictureAspect: "noChange",
+      assets: {
+        ios6AndPriorIcons: false,
+        ios7AndLaterIcons: false,
+        precomposedIcons: false,
+        declareOnlyDefaultIcon: true,
+      },
+    },
+    desktopBrowser: {
+      design: "raw",
+    },
+    windows: {
+      pictureAspect: "noChange",
+      backgroundColor: theme.colors.brand[500],
+      onConflict: "override",
+      assets: {
+        windows80Ie10Tile: false,
+        windows10Ie11EdgeTiles: {
+          small: false,
+          medium: true,
+          big: false,
+          rectangle: false,
+        },
+      },
+    },
+    androidChrome: {
+      pictureAspect: "noChange",
+      themeColor: "#ffffff",
+      manifest: {
+        name: "Open FPL",
+        display: "standalone",
+        orientation: "notSet",
+        onConflict: "override",
+        declared: true,
+      },
+      assets: {
+        legacyIcon: false,
+        lowResolutionIcons: false,
+      },
+    },
+    safariPinnedTab: {
+      pictureAspect: "blackAndWhite",
+      threshold: 65.625,
+      themeColor: theme.colors.brand[500],
+    },
+  },
+  settings: {
+    scalingAlgorithm: "Mitchell",
+    errorOnImageTooSmall: false,
+    readmeFile: false,
+    htmlCodeFile: false,
+    usePathAsIs: false,
+  },
+};
+
+const makeCliRealFavIconConfig = async () => {
+  return fs.promises.writeFile(
+    "./config.cli-real-favicon.json",
+    JSON.stringify(config, null, 2),
+    "utf8"
+  );
+};
+
+(async function () {
+  const start = new Date();
+  await makeCliRealFavIconConfig();
+})();
