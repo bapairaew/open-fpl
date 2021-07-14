f088ffc0f5277ee43a07ff8f344b2f93197c0747
Narudom <iam@bapairaew.com>
2021-07-14 15:36:03 +0700

    Fix missing favicons tasks and wrong env var

--- features/Navigation/internalUrls.ts
+++ features/Navigation/internalUrls.ts
@@ -1,3 +1,3 @@
-export const baseUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:300";
+export const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  : "http://localhost:3000";
