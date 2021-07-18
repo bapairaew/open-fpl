5ed0c83c05cd0c3da722340e0fdcdb10ae1bcd7c
Narudom <iam@bapairaew.com>
2021-07-18 20:13:06 +0700

    Add license, improve data index page ui

--- packages/data/pages/index.tsx
+++ packages/data/pages/index.tsx
@@ -15,7 +15,32 @@ const IndexPage = ({
   return (
     <>
       <NextSeo noindex title="Open FPL Data" />
-      <div>Last update: {date}</div>
+      <style jsx>{`
+        main {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          flex-direction: column;
+          font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto;
+          padding: 2vw;
+          height: 96vh;
+          width: 96vw;
+        }
+        h1 {
+          opacity: 0.7;
+          font-size: 4vw;
+          margin: 0 0 40px 0;
+        }
+        p {
+          font-weight: 900;
+          font-size: 5vw;
+          margin: 0;
+        }
+      `}</style>
+      <main>
+        <h1>Last update</h1>
+        <p>{date}</p>
+      </main>
     </>
   );
 };
