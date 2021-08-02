6b08006a4b8c63aa00ca685da646e8dde4bfa580
Narudom <iam@bapairaew.com>
2021-08-03 06:27:51 +0700

    Fix invalid light mode color

--- packages/app/features/TeamData/teamData.ts
+++ packages/app/features/TeamData/teamData.ts
@@ -18,91 +18,91 @@ export const teamColorCodes: (mode: string) => Record<string, TeamColorCode> = (
   mode
 ) => ({
   ARS: {
-    bg: transparentize("#fe0102", 0.6)(theme),
+    bg: transparentize("#fe0102", mode === "dark" ? 0.6 : 1)(theme),
     color: "#ffffff",
   },
   BRE: {
-    bg: transparentize("#c61d23", 0.6)(theme),
+    bg: transparentize("#c61d23", mode === "dark" ? 0.6 : 1)(theme),
     color: "#ffffff",
   },
   LIV: {
-    bg: transparentize("#e31b23", 0.6)(theme),
+    bg: transparentize("#e31b23", mode === "dark" ? 0.6 : 1)(theme),
     color: "#ff9065",
   },
   MUN: {
-    bg: transparentize("#d81920", 0.6)(theme),
+    bg: transparentize("#d81920", mode === "dark" ? 0.6 : 1)(theme),
     color: "#ffe400",
   },
   SOU: {
-    bg: transparentize("#d71920", 0.6)(theme),
+    bg: transparentize("#d71920", mode === "dark" ? 0.6 : 1)(theme),
     color: "#ffffff",
   },
   BUR: {
-    bg: transparentize("#6a003a", 0.6)(theme),
+    bg: transparentize("#6a003a", mode === "dark" ? 0.6 : 1)(theme),
     color: "#fff216",
   },
   WHU: {
-    bg: transparentize("#7d2c3b", 0.6)(theme),
+    bg: transparentize("#7d2c3b", mode === "dark" ? 0.6 : 1)(theme),
     color: "#ffffff",
   },
 
   CRY: {
-    bg: transparentize("#1d3c93", 0.6)(theme),
+    bg: transparentize("#1d3c93", mode === "dark" ? 0.6 : 1)(theme),
     color: "#eb302e",
   },
 
   CHE: {
-    bg: transparentize("#0a4495", 0.6)(theme),
+    bg: transparentize("#0a4495", mode === "dark" ? 0.6 : 1)(theme),
     color: "#ffffff",
   },
   BHA: {
-    bg: transparentize("#0054a6", 0.6)(theme),
+    bg: transparentize("#0054a6", mode === "dark" ? 0.6 : 1)(theme),
     color: "#ffffff",
   },
   EVE: {
-    bg: transparentize("#124080", 0.6)(theme), // 00369c
+    bg: transparentize("#124080", mode === "dark" ? 0.6 : 1)(theme), // 00369c
     color: "#ffffff",
   },
   LEI: {
-    bg: transparentize("#0b56a4", 0.6)(theme), // 273e8a
+    bg: transparentize("#0b56a4", mode === "dark" ? 0.6 : 1)(theme), // 273e8a
     color: "#ffffff",
   },
   TOT: {
-    bg: transparentize("#00164d", 0.6)(theme),
+    bg: transparentize("#00164d", mode === "dark" ? 0.6 : 1)(theme),
     color: "#ffffff",
   },
 
   LEE: {
-    bg: transparentize("#084893", 0.6)(theme),
+    bg: transparentize("#084893", mode === "dark" ? 0.6 : 1)(theme),
     color: "#f9ea02",
   },
 
   MCI: {
-    bg: transparentize("#6caee0", 0.6)(theme),
+    bg: transparentize("#6caee0", mode === "dark" ? 0.6 : 1)(theme),
     color: "#ffffff",
   },
 
   AVL: {
-    bg: transparentize("#94bee5", 0.6)(theme),
+    bg: transparentize("#94bee5", mode === "dark" ? 0.6 : 1)(theme),
     color: "#480025",
   },
 
   NEW: {
-    bg: transparentize("#000000", 0.6)(theme),
+    bg: transparentize("#000000", mode === "dark" ? 0.6 : 1)(theme),
     color: "#ffffff",
   },
 
   NOR: {
-    bg: transparentize("#fff200", 0.6)(theme),
+    bg: transparentize("#fff200", mode === "dark" ? 0.6 : 1)(theme),
     color: "#00a651",
   },
   WAT: {
-    bg: transparentize("#ffee02", 0.6)(theme),
+    bg: transparentize("#ffee02", mode === "dark" ? 0.6 : 1)(theme),
     color: "#000000",
   },
 
   WOL: {
-    bg: transparentize("#f9a01b", 0.6)(theme),
+    bg: transparentize("#f9a01b", mode === "dark" ? 0.6 : 1)(theme),
     color: "#000000",
   },
 });
