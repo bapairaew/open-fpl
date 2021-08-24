6c6baa4327accf6548c1cf914071b466dcf28fc7
Narudom <iam@bapairaew.com>
2021-08-24 13:36:55 +0700

    Remove invalid page description

--- packages/app/pages/500.tsx
+++ packages/app/pages/500.tsx
@@ -6,11 +6,7 @@ import { NextSeo } from "next-seo";
 export default function Custom500() {
   return (
     <>
-      <NextSeo
-        title="Something went wrong – Open FPL"
-        description="This page does not exist."
-        noindex
-      />
+      <NextSeo title="Something went wrong – Open FPL" noindex />
       <AppLayout>
         <UnhandledError
           Wrapper={FullScreenMessageWithAppDrawer}
